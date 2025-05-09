ece5a53b2b9a28246197e1bedcd75e99ae8a0f22
OrcaX <jbl01570@hanmail.net>
2025-05-09 17:54:46 +0900

    Update apply.js
    Signed-off-by: OrcaX <jbl01570@hanmail.net>

--- apply.js
+++ apply.js
@@ -13,13 +13,13 @@ document.getElementById("applyForm").addEventListener("submit", async function (
     const result = await res.json();
 
     if (res.ok && result.success) {
-      resultElement.textContent = "✅ 신청 완료! 범고래 접수 완료!";
-      resultElement.style.color = "green";
-      form.reset();
-    } else {
-      resultElement.textContent = "❌ 오류: " + (result.message || "신청 실패");
-      resultElement.style.color = "red";
-    }
+  resultElement.textContent = "✅ 신청 완료! 범고래 접수 완료!";
+  resultElement.style.color = "green";
+  form.reset();
+} else {
+  resultElement.textContent = "❌ 오류: " + (result.message || "신청 실패");
+  resultElement.style.color = "red";
+}
   } catch (error) {
     console.error("❌ 네트워크 오류:", error);
     resultElement.textContent = "❌ 서버에 연결할 수 없습니다.";
