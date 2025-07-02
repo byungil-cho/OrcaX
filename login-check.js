757868468174b94c6e1f898e006541585016befc
OrcaX <jbl01570@hanmail.net>
2025-07-02 21:53:18 +0900

    Add files via upload
    Signed-off-by: OrcaX <jbl01570@hanmail.net>

---
+++ login-check.js
@@ -0,0 +1,13 @@
+// login-check.js
+export function getUserInfoOrRedirect() {
+  const nickname = localStorage.getItem("nickname");
+  const kakaoId = localStorage.getItem("kakaoId");
+
+  if (!nickname || !kakaoId) {
+    alert("로그인이 필요합니다.");
+    window.location.href = "https://byungil-cho.github.io/OrcaX/kakao-callback.html";
+    return {};
+  }
+
+  return { nickname, kakaoId };
+}
